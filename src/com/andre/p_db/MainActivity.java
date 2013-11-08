dde42fd76e297ac0fe8f1156f90f656ea1623e5c
mormul <andrey.mormul@gmail.com>
2013-11-08 14:04:55 +0200

    buttons are not needed

--- src/com/andre/p_db/MainActivity.java
+++ src/com/andre/p_db/MainActivity.java
@@ -5,25 +5,33 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity implements OnClickListener,
+		OnTouchListener {
 	DBH dbh;
 	SQLiteDatabase db;
 	Button btnl, btnr, btna;
 	EditText et;
-	TextView tv;
+	TextView tv, tvstatus;
 	int pos = 0;
 	Cursor c;
+	LinearLayout mainid;
+	float x, y;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		mainid = (LinearLayout) findViewById(R.id.mainid);
+		mainid.setOnTouchListener(this);
 		btnl = (Button) findViewById(R.id.btnl);
 		btnr = (Button) findViewById(R.id.btnr);
 		btna = (Button) findViewById(R.id.btna);
@@ -33,11 +41,12 @@ public class MainActivity extends Activity implements OnClickListener {
 		btna.setOnClickListener(this);
 		et = (EditText) findViewById(R.id.et1);
 		tv = (TextView) findViewById(R.id.tv1);
+		tvstatus = (TextView) findViewById(R.id.tvstatus);
 		dbh = new DBH(this);
-
+et.setOnTouchListener(this);
 		echo(pos);
 		btna.setEnabled(false);
-		
+
 	}
 
 	public boolean echo(int pos) {
@@ -48,7 +57,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			c.moveToFirst();
 			et.setText(c.getString(1));
 			tv.setText(c.getString(0));
-		
+
 			pos = 1;
 		} else {
 			pos = 0;
@@ -56,10 +65,11 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 
-	private void isClose(){
+	private void isClose() {
 		if ((c != null) && (c.isClosed() == false))
 			c.close();
 	}
+
 	public void onDestroy() {
 		this.isClose();
 		if (db.isOpen())
@@ -73,6 +83,27 @@ public class MainActivity extends Activity implements OnClickListener {
 		return true;
 	}
 
+	private void _btnl() {
+		if ((c != null) && (pos >= 1)) {
+			pos--;
+			c.moveToPrevious();
+			tv.setText(c.getString(0));
+			et.setText(c.getString(1));
+
+		}
+	}
+
+	private void _btnr() {
+		if ((c != null) && (c.getCount() > (pos + 1))) {
+			pos++;
+			c.moveToNext();
+			tv.setText(c.getString(0));
+			et.setText(c.getString(1));
+
+		}
+
+	}
+
 	@Override
 	public void onClick(View arg0) {
 
@@ -83,25 +114,38 @@ public class MainActivity extends Activity implements OnClickListener {
 			// db.insert("mytable", null, cv);
 			break;
 		case R.id.btnr:
-			if ((c != null) && (c.getCount() > (pos + 1))) {
-				pos++;
-				c.moveToNext();
-				tv.setText(c.getString(0));
-				et.setText(c.getString(1));
-		
-
-			}
+			_btnr();
 			break;
 		case R.id.btnl:
-			if ((c != null) && (pos >= 1)) {
-				pos--;
-				c.moveToPrevious();
-				tv.setText(c.getString(0));
-				et.setText(c.getString(1));
-		
-			}
+			_btnl();
 			break;
 		}
 		;
+	}
+
+	@Override
+	public boolean onTouch(View arg0, MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			tvstatus.setText("down  " + x + "  " + y);
+			x = event.getX();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			tvstatus.setText("move  " + x + "  " + y);
+			break;
+		case MotionEvent.ACTION_UP:
+			if (event.getX() < x) {
+				tvstatus.setText("REV");
+				_btnl();
+			} else {
+				tvstatus.setText("NEXT");
+				_btnr();
+			}
+			if (event.getX() == x) {
+				tvstatus.setText("no move");
+			}
+			break;
+		}
+		return true;
 	}
 }
